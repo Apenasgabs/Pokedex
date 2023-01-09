ca21d98120de6844162785df7fc1788c6be75337
Gabriel Rodrigues <Gabers357@gmail.com>
2023-01-09 16:58:33 -0300

    removido alguns componetes #03

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import { searchPokemon } from "./api";
 import "./App.css";
-import Navbar from "./components/Navbar";
-import Searchbar from "./components/Searchbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Navbar />
-        <Searchbar />
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }
